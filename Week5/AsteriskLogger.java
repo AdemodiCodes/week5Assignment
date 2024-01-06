5bf71e5725dec7f9e8db6f69ce76d9fb31a8cc29
AdemodiCodes <csprogrammer.ademodi@outlook.com>
2024-01-05 21:35:33 -0600

    Update AsteriskLogger.java

--- Week5/AsteriskLogger.java
+++ Week5/AsteriskLogger.java
@@ -1,11 +1,11 @@
 package Week5;
 
-class AsteriskLogger implements Logger {
-    public void log(String message) {
-        System.out.println("***" + message + "***");
+class AsteriskLogger implements Logger { //Implements Logger interface in App. //Asterisklogger Class
+    public void log(String message) { //String Method
+        System.out.println("***" + message + "***"); //Asterisks then inserts message followed by asterisks.
     }
     @Override
     public void error(String message) {
-        System.out.println("****************\n***Error: " + message + "***\n****************");
+        System.out.println("****************\n***Error: " + message + "***\n****************"); //Same as above but error added Asterisks then inserts message followed by asterisks.
     }
 }
